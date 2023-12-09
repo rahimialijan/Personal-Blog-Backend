40f00e2fc95057f48e047665a6eb769d77b9b967
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-12-09 17:26:32 +0500

    install gem-cors and configured the cors.rb file for frontend

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,4 @@ Rails.application.routes.draw do
       resources :posts
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
