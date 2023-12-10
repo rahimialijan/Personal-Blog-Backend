40923a2a853050fd24113b7cc420ca2a041ccd54
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-12-10 22:24:28 +0500

    fix the linters

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,48 +1,46 @@
 class Api::V1::PostsController < ApplicationController
-    before_action :set_post, only: [:show, :update, :destroy]
+  before_action :set_post, only: %i[show update destroy]
 
 
-    def index 
-        @posts =Post.all
-        render json: @posts
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    render json: @posts
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
+  end
 
-
-    def show
-        render json: @posts
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @post = Post.new(post_params)
-    
-        if @post.save
-          render json: @post, status: :created
-        else
-          render json: @post.errors, status: :unprocessable_entity
-        end
-      end
+  def destroy
+    @post.destroy
+    head :no_content
+  end
 
-    def update
-        if @post.update(post_params)
-            render json: @post
-        else
-            render json: @post.errors, status: :unprocessable_entity
-        end
-    end
+  private
 
-    def destroy
-        @post.destroy
-        head :no_content
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    private
-
-    def set_post
-        @post = Post.find(params[:id])
-      end
-
-    def post_params
-        params.require(:post).permit(:title, :author, :content)
-    end
-      
+  def post_params
+    params.require(:post).permit(:title, :author, :content)
+  end
 end
